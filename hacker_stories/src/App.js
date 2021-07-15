beb811c2e522f2cddc1cbc9e1c21ac632cf6ef83
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-07-15 12:47:24 +0530

    experimented jsx

--- hacker_stories/src/App.js
+++ hacker_stories/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const title="React";
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+function getTitle(title){
+  return title;
+}
+
 function App() {
   return (
     <div>
       <h1>
-        Hello World
+        Hello {getTitle('React from facebook')}
       </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
