858ac0dab019aef01c441c43721849fa5eb512b1
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-07-15 12:55:10 +0530

    experimented jsx

--- hacker_stories/src/App.js
+++ hacker_stories/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <h1>
-        Hello {getTitle('React from facebook')}
+        Hello {getTitle('React')}
       </h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text"/>
