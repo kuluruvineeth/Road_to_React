1c56e36c906cc12657d655284c7a464ffae6cfce
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-07-15 12:38:16 +0530

    initial react project setup

---
+++ hacker_stories/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const title="React";
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
